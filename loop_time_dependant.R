5bfabcc26ed0089c9b90a63e96d6f3a2104f5a96
Rachel <radub33@ulaval.ca>
2023-07-27 11:20:07 -0400

    Automatically change the col names of the output df

--- loop_time_dependant.R
+++ loop_time_dependant.R
@@ -26,8 +26,6 @@ result_df <- generate_parameter_dataframe()
 # every other computation should be run
 
 
-result_df <- generate_parameter_dataframe()
-
 test <- result_df[1:3,]
 
 nested_test <- test %>% 
@@ -76,14 +74,36 @@ for (i in 1:nrow(test)){
 
 # Deal with the list and maybe convert to df
 
+# library(purrr)
+# library(dplyr)
+# library(stringr)
+# 
+# for (i in 1:nrow(test)){
+# imap(res, ~ set_names(tibble(.x), .y))} %>%
+#   set_names(str_c("DF", 1:nrow(test))) %>% 
+#   list2env(.GlobalEnv)
+}
+
 library(purrr)
 library(dplyr)
-library(stringr)
 
-for (i in 1:nrow(test)){
-imap(res, ~ set_names(tibble(.x), .y)) %>%
-  set_names(str_c("DF", 1:nrow(test))) %>% 
-  list2env(.GlobalEnv)
-}
+# Assuming 'res' is the list of data frames you want to modify
+# You can use the following code:
+
+# Define the column names you want for each data frame
+col_names <- c("time", "k_U", "k_V")
+
+# Use 'map' to modify each data frame and rename columns
+modified_dfs <- map(res, ~ {
+  df <- set_names(tibble(.x), col_names) # Rename columns using 'col_names'
+  return(df)
+})
+
+# Rename the list elements to match the desired names (e.g., DF1, DF2, ...)
+modified_dfs <- set_names(modified_dfs, str_c("DF", 1:length(modified_dfs)))
+
+# Optionally, if you want to save the modified data frames to the global environment
+list2env(modified_dfs, .GlobalEnv)
+
 
 
