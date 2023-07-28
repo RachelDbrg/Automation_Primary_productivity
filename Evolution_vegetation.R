2f29431d30c8462598de8918fa5792c5b5a81637
Rachel <radub33@ulaval.ca>
2023-07-28 10:48:52 -0400

    Function that is called in the species equations.

--- Evolution_vegetation.R
+++ Evolution_vegetation.R
@@ -3,22 +3,37 @@
 
   # Calcul les evolutions des capacites de charge en fct 
   # de PP et du temps
-evol_vg <- function(t,data){
-  
-  
+evol_vg <- function(t,data,PP){
+ 
+
+  # Need to specify where to find the columns
+  # where the values are stored
+  kUstable <- data$kUstable
+  kUpeak <- data$kUpeak
+  t_kstable <- data$t_kstable
+  t_kpeak <- data$t_kpeak
+  t_low <- data$t_low
+  kWstable <- data$kWstable
+  kWpeak <- data$kWpeak
+  t_perturb <- data$t_perturb
+  kUcoeff1 <- data$kUcoeff1
+  kUcoeff2 <- data$kUcoeff2
+  kWcoeff1 <- data$kWcoeff1
+  kWcoeff2 <- data$kWcoeff2
+
   
 # t = 1500
-# #Perturbation :
-# # Definition du moment de la perturbation
-# t_pertub = 100            # years
-# # #t_pertub = 0
-# 
+#Perturbation :
+# Definition du moment de la perturbation
+  # t_pertub = 100            # years
+
+  
 # # Definition des moments suivant la perturbation
-# t_low = t_pertub + 5           # years : temps pour atteindre le minimum de
+#   t_low = t_perturb + 5           # years : temps pour atteindre le minimum de
 # # biomasse apres une perturbation
-# t_kpeak = t_low + (50 - 25*PP) # years :  temps pour atteindre le maximum de
+#   t_kpeak = t_low + (50 - 25*PP) # years :  temps pour atteindre le maximum de
 # # biomasse apres une perturbation
-# t_kstable = t_kpeak + 100      # years : temps pour atteindre une stabilité de
+#   t_kstable = t_kpeak + 100      # years : temps pour atteindre une stabilité de
 # # biomasse apres une perturbation
 
   
@@ -27,13 +42,13 @@ evol_vg <- function(t,data){
     
     # Coefficients for the linear variation of k_U and k_W between t_peak and
   # t_stable
-  kUcoeff1 = (kUstable - kUpeak)/(t_kstable - t_kpeak)
-  kUcoeff2 = kUpeak - (kUstable - kUpeak)/(t_kstable - t_kpeak) * t_kpeak
+  # kUcoeff1 = (kUstable - kUpeak)/(t_kstable - t_kpeak)
+  # kUcoeff2 = kUpeak - (kUstable - kUpeak)/(t_kstable - t_kpeak) * t_kpeak
   # kWcoeff1 = (kWstable - kWpeak)/(t_kstable - t_kpeak)
   # kWcoeff2 = kWpeak - (kWstable - kWpeak)/(t_kstable - t_kpeak) * t_kpeak
-  
-  
-   if (t_pertub <= t & t < t_low) {
+
+  # print(t)
+   if (t_perturb <= t & t < t_low) {
     # k_U = 0
     # k_W = 0
     # k_V = 0
@@ -64,6 +79,7 @@ evol_vg <- function(t,data){
   }
   # return(list(c(k_U,k_W,k_V)))
   res <- c(k_U,k_W,k_V)
+  # res <- c(k_U,k_V)
   
   
 }
