25bebd1af9711f1a2f706f3b1126a814b6bd8e43
Rachel <radub33@ulaval.ca>
2023-07-28 10:49:33 -0400

    Script where the make_ODE function is applied to our df

--- Auto_ODE_final.R
+++ Auto_ODE_final.R
@@ -1,22 +1,39 @@
+
+rm(list = ls(all.names = TRUE))
+
 # on several PP values
 source("Auto_gen_PP_related_parameters.R")
 
-
 source("Static_vegetation_parameters.R")
 
-source("Time_dependant_parameters.R")
+# source("Time_dependant_parameters.R")
 
 # source("Parametres_animaux_deers.R")
 
 source("Static_fauna_parameters.R")
 
-# source("Evolution_vegetation.R")
+source("Evolution_vegetation.R")
 
 source("test_model_equations.R")
 
 source("intermediate_res.R")
 
-nested_test <- test %>% 
+source("make_ODE_function.R")
+
+source("Species_equations.R")
+
+result_df <- generate_parameter_dataframe()
+
+
+nested_test <- result_df %>% 
   group_by(PP) %>% 
-  nest()
+  nest() %>% 
+  head(3)
+
+
+# Apply the ODE solver
+test <- nested_test %>% 
+  ungroup() %>% 
+  mutate(outputs = map2(nested_test$PP, nested_test$data, ~make_ODE(.x, .y)))
+  # mutate(outputs = make_ODE(PP, data))
 
