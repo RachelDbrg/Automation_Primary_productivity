6fab60caacda30ffb21c5e71fe70e91f61ad1e9b
Rachel <radub33@ulaval.ca>
2023-08-11 11:54:40 -0400

    Run the ODE function

--- scripts/Auto_ODE_final.R
+++ scripts/Auto_ODE_final.R
@@ -1,6 +1,9 @@
 
 rm(list = ls(all.names = TRUE))
 
+library(deSolve)
+
+
 # on several PP values
 source("Auto_gen_PP_related_parameters.R")
 
@@ -31,20 +34,21 @@ result_df <- generate_parameter_dataframe()
 
 nested_test <- result_df %>% 
   group_by(PP) %>% 
-  nest() %>% 
-  head(1)
+  nest()
 
 
 # Apply the ODE solver
-test <- nested_test %>% 
+res <- nested_test %>% 
   ungroup() %>% 
   mutate(outputs = map2(nested_test$PP, nested_test$data, ~make_ODE(.x, .y)))
   # mutate(outputs = make_ODE(PP, data))
 
+saveRDS(res, file = "all_simulations.R")
 
 
-test_model_auto <- test$outputs[[1]] %>% 
-  filter(time == 666)
 
-# Check si les datas sont les memes 
-data_test_auto <- test_model_auto <- test$data[[1]]
+# test_model_auto <- test$outputs[[1]] %>% 
+#   filter(time == 666)
+# 
+# # Check si les datas sont les memes 
+# data_test_auto <- test_model_auto <- test$data[[1]]
