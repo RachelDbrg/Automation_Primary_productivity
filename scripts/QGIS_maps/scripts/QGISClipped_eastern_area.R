387da45ca7df5049b7744f288d5805d1fd2ff126
Rachel <radub33@ulaval.ca>
2023-09-18 13:26:31 -0400

    useless

--- scripts/QGIS_maps/scripts/QGISClipped_eastern_area.R
+++ scripts/QGIS_maps/scripts/QGISClipped_eastern_area.R
@@ -1,1 +1,15 @@
+library(tidyverse)
+library(readr)
 
+# Import the file where only wolf within the clipped area are retained
+
+
+wolves_within_eastern_area <- read_csv("~/wolves_within_eastern_area.csv")
+
+
+# How many wolves were recorded in this area?
+
+wolves_within_eastern_area %>% 
+  group_by(id, year) %>% 
+  count() %>% 
+  View()
